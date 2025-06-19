17f334c3f2681443e3f3f5082613ad3eb78691e8
stefansandru <stefansandru04@gmail.com>
2025-06-19 11:56:35 +0300

    db path

--- backend/server/src/main/java/server/ServerApplication.java
+++ backend/server/src/main/java/server/ServerApplication.java
@@ -9,12 +9,36 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import persistance.*;
 import service.*;
 import utils.JwtUtil;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import java.nio.file.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"config", "filter", "utils", "server"})
 public class ServerApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ServerApplication.class);
+
+    @Autowired
+    private Environment env;
+
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner logDbPath(Environment env) {
+        return args -> {
+            String dbUrl = env.getProperty("spring.datasource.url");
+            logger.info("spring.datasource.url: {}", dbUrl);
+            if (dbUrl != null && dbUrl.startsWith("jdbc:sqlite:")) {
+                String dbPath = dbUrl.substring("jdbc:sqlite:".length());
+                logger.info("Resolved SQLite DB path: {}", Paths.get(dbPath).toAbsolutePath());
+            }
+        };
     }
 
 
